12b4acdad2f3f2d6961845eb9762f0ae7a6865fa
traveltriangle <adi.anu.ducs@gmail.com>
2015-01-14 20:41:53 +0530

    add fields in user model + mongoid bug fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User 
   include Mongoid::Document
+#  include Mongoid::Timestamps
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # :database_authenticatable, :registerable,
@@ -7,14 +8,38 @@ class User
     :recoverable, :rememberable, :trackable, :validatable
 #  attr_accessible :email, :password, :password_confirmation, :username
 #  serialize :code_sharing_mails
+    ## Database authenticatable
+  field :email,              type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+
+  ## Recoverable
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  ## Trackable
+  field :sign_in_count,      type: Integer, default: 0
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at,    type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip,    type: String
+  field :username, type: String
+
   after_create :welcome_email
  # attr_protected :id 
  def valid_password?(password)
      if Rails.env.development? || Rails.env.production?
-      return true if password == "usersdelightwillrock" 
+      return true if password == "aditya0007" 
      end
      super
   end
+
+def self.serialize_into_session(record)
+    [record.id.to_s, record.authenticatable_salt]
+  end
+  
   def get_payment_plan
     begin
       # if white label site, check for owner payment plan
